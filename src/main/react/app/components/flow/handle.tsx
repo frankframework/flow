0a1bf9c908d7f2d4d9227e91b2a10a31a4b40be0
Daan <74923097+Daan0709@users.noreply.github.com>
2025-05-12 10:09:52 +0200

    Increased interactivity with handles and forwards (#52)
    * Added functionability for editing and deleting nodes
    
    Added nodrag to certain components to prevent unwanted dragging
    
    * Can now add sticky notes to the canvas by right mouse clicking anywhere
    
    * Bugfix for width not showing properly after switching routes
    
    * Bugfix for width not showing properly after switching routes
    
    * Bugfix for width not showing properly after switching routes on exitnode
    
    * Extra bugfix commit courtesty to gitbutler
    
    * Edges now get labels based on their origin handle, also added a delete button when the edge gets selected
    
    * Fixed bug where the minimum height would set to the current height after switching pages, attributes now no longer spill out of the bottom of a node and get hidden instead
    
    * Duplicated code
    
    * Can now change the type of the handle (and thus forward)
    
    * Can now only connect 1 edge to a source handle
    
    * Can now detach edges from handles

--- src/main/react/app/components/flow/handle.tsx
+++ src/main/react/app/components/flow/handle.tsx
@@ -1,10 +1,13 @@
-import { Handle, Position } from '@xyflow/react'
+import { Handle, Position, useNodeConnections, useReactFlow } from '@xyflow/react'
+import { useState } from 'react'
 
 interface HandleProperties {
   type: string
   index: number
   firstHandlePosition: number
   handleSpacing: number
+  onChangeType: (newType: string) => void
+  absolutePosition: { x: number; y: number }
 }
 
 function translateHandleTypeToColour(type: string): string {
@@ -28,22 +31,98 @@ function translateHandleTypeToColour(type: string): string {
 }
 
 export function CustomHandle(properties: Readonly<HandleProperties>) {
-  const bgColor = translateHandleTypeToColour(properties.type)
+  const connections = useNodeConnections({ handleType: 'source', handleId: properties.index.toString() })
+  const [type, setType] = useState(properties.type)
+  const reactFlow = useReactFlow()
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [menuPosition, setMenuPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 })
+
+  const handleClick = (event: React.MouseEvent) => {
+    const { clientX, clientY } = event
+    const { screenToFlowPosition } = reactFlow
+    const flowPosition = screenToFlowPosition({ x: clientX, y: clientY })
+    setMenuPosition({
+      x: flowPosition.x - properties.absolutePosition.x,
+      y: flowPosition.y - properties.absolutePosition.y,
+    })
+    setIsMenuOpen(!isMenuOpen) // Toggle menu visibility
+  }
+
+  const handleMenuClick = (newType: string) => {
+    properties.onChangeType(newType) // Change the handle type
+    setIsMenuOpen(false) // Close the menu after selection
+  }
+
   return (
-    <Handle
-      type={'source'}
-      position={Position.Right}
-      key={properties.index}
-      id={properties.index.toString()}
-      className="flex items-center justify-center text-white"
-      style={{
-        top: `${properties.firstHandlePosition + properties.index * properties.handleSpacing}px`,
-        right: '-15px',
-        width: '15px',
-        height: '15px',
-        backgroundColor: bgColor,
-        border: '1px solid rgba(107, 114, 128, 0.5)',
-      }}
-    />
+    <div>
+      <div>
+        <Handle
+          type={'source'}
+          position={Position.Right}
+          key={properties.index}
+          id={properties.index.toString()}
+          className="flex cursor-pointer items-center justify-center text-white"
+          isConnectableStart={connections.length === 0}
+          isConnectableEnd={connections.length === 0}
+          onClick={handleClick}
+          style={{
+            top: `${properties.firstHandlePosition + properties.index * properties.handleSpacing}px`,
+            right: '-15px',
+            width: '15px',
+            height: '15px',
+            backgroundColor: translateHandleTypeToColour(type),
+            border: '1px solid rgba(107, 114, 128, 0.5)',
+            pointerEvents: 'all',
+          }}
+        />
+      </div>
+
+      {isMenuOpen && (
+        <div
+          className="nodrag absolute rounded-md border bg-white shadow-md"
+          style={{
+            left: `${menuPosition.x + 10}px`,
+            top: `${menuPosition.y + 10}px`,
+          }}
+        >
+          <ul>
+            <button
+              className="absolute -top-1 -right-1 rounded-full border border-gray-300 bg-white text-gray-400 shadow-sm hover:border-red-400 hover:text-red-400"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-3 w-3"
+                width="10"
+                height="10"
+                viewBox="0 0 10 10"
+                strokeWidth="1"
+                stroke="currentColor"
+                strokeLinecap="round"
+              >
+                <line x1="3" y1="3" x2="7" y2="7" />
+                <line x1="3" y1="7" x2="7" y2="3" />
+              </svg>
+            </button>
+            <li
+              className="cursor-pointer rounded-t-md p-2 hover:bg-gray-200"
+              onClick={() => handleMenuClick('success')}
+            >
+              Success
+            </li>
+            <li className="cursor-pointer p-2 hover:bg-gray-200" onClick={() => handleMenuClick('failure')}>
+              Failure
+            </li>
+            <li className="cursor-pointer p-2 hover:bg-gray-200" onClick={() => handleMenuClick('exception')}>
+              Exception
+            </li>
+            <li className="cursor-pointer rounded-b-md p-2 hover:bg-gray-200" onClick={() => handleMenuClick('custom')}>
+              Custom
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   )
 }
