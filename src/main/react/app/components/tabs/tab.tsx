827d75999657f91f7c0c15ee178228cd0c172481
Sergi Philipsen <philipsen.sergi@gmail.com>
2025-05-19 22:31:07 +0200

    Add theme example and suddenly add some yellow to the tabs

--- src/main/react/app/components/tabs/tab.tsx
+++ src/main/react/app/components/tabs/tab.tsx
@@ -17,8 +17,8 @@ export default function Tab({ value, icon, isSelected, onSelect, onClose }: Read
       className={clsx(
         'group relative flex h-full rotate-x-180 list-none items-center justify-between gap-1 border-r border-b border-r-gray-200 border-b-gray-200 px-4 text-gray-500',
         isSelected
-          ? 'border-b-white bg-white font-medium text-gray-950 hover:bg-white'
-          : 'bg-gray-50 hover:cursor-pointer hover:bg-gray-100 hover:text-gray-800',
+          ? 'border-t-3 border-t-[var(--color-brand)] border-b-white bg-white font-medium text-gray-950 hover:bg-white'
+          : 'border-t-3 border-t-transparent bg-gray-50 hover:cursor-pointer hover:bg-gray-100 hover:text-gray-800',
       )}
       onClick={onSelect}
     >
