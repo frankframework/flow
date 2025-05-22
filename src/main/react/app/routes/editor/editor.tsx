7dc94b2a41c446eea2306f492cb8424d55ac26a7
Sergi Philipsen <philipsen.sergi@gmail.com>
2025-05-22 21:38:52 +0200

    Also implement darkmode for the Monaco Editor

--- src/main/react/app/routes/editor/editor.tsx
+++ src/main/react/app/routes/editor/editor.tsx
@@ -8,6 +8,7 @@ import SidebarLayout from '~/components/sidebars-layout/sidebar-layout'
 import { SidebarSide } from '~/components/sidebars-layout/sidebar-layout-store'
 import SidebarContentClose from '~/components/sidebars-layout/sidebar-content-close'
 import { useSettingsStore } from '~/routes/settings/settings-store'
+import { useTheme } from '~/hooks/use-theme'
 
 const tabs = {
   tab1: { value: 'tab1', icon: FolderIcon },
@@ -23,10 +24,8 @@ const tabs = {
 } as TabsList
 
 export default function CodeEditor() {
-  const monaco = useMonaco();
+  const theme = useTheme()
   const [selectedTab, setSelectedTab] = useState<string | undefined>()
-  const theme  = useSettingsStore(state => state.general.theme)
-  const monacoTheme = theme === 'light'  ? 'vs-dark' : 'vs-light'
 
   return (
     <SidebarLayout name="editor" windowResizeOnChange={true}>
@@ -45,7 +44,7 @@ export default function CodeEditor() {
           Path: {Object.entries(tabs).find(([key]) => key === selectedTab)?.[1]?.value}
         </div>
         <div className="h-full">
-          <Editor language="xml" theme={monacoTheme}></Editor>
+          <Editor language="xml" theme={`vs-${theme}`}></Editor>
         </div>
       </>
       <>
