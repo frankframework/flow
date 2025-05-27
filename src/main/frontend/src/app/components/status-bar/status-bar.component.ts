954edc97302f0206b14121598abf2f3162f33fa9
Sergi Philipsen <philipsen.sergi@gmail.com>
2025-05-27 13:07:28 +0200

    Remove Angular (#75)

--- src/main/frontend/src/app/components/status-bar/status-bar.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-status-bar',
-  imports: [],
-  templateUrl: './status-bar.component.html',
-  styleUrl: './status-bar.component.scss',
-})
-export class StatusBarComponent {}
--- src/main/frontend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-// eslint-disable-next-line unicorn/prefer-top-level-await
-bootstrapApplication(AppComponent, appConfig).catch((error) => console.error(error));
