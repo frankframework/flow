9c9e98a4619279d6a7b246fd9bd0051ecfc89878
Daan <daanvmaldegem@live.nl>
2025-10-28 13:08:31 +0100

    Implement ff-doc library (#112)

--- src/main/frontend/app/routes/builder/context/sorted-elements.tsx
+++ src/main/frontend/app/routes/builder/context/sorted-elements.tsx
@@ -33,12 +33,12 @@ export default function SortedElements({ type, items, onDragStart, searchTerm }:
     >
       <button
         onClick={toggleExpansion}
-        className="flex w-full cursor-pointer items-center gap-1 text-left text-sm font-semibold text-foreground-muted capitalize hover:text-gray-900"
+        className="flex w-full cursor-pointer items-center gap-1 text-left text-sm font-semibold text-foreground-muted capitalize hover:text-foreground-active"
       >
         {shouldExpand ? (
-          <ArrowDownIcon className="fill-foreground-muted" />
+          <ArrowDownIcon className="fill-current" />
         ) : (
-          <ArrowRightIcon className="fill-foreground-muted" />
+          <ArrowRightIcon className="fill-current" />
         )}
         {type === 'other' ? type : `${type}s`}
       </button>
