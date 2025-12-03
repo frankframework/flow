324401d3107a5461bc13822cc59352707ce8219c
Daan0709 <daanvmaldegem@live.nl>
2025-12-03 17:20:17 +0100

    Unit tests (#135)
    * Attempt at autocompleting frankdoc elements
    
    * Editor now suggests frank-elements when opening a tag: '<' and inserts any mandatory attributes when the suggestion is inserted. Also suggests attribute names when typing within a tag
    
    * Doesnt show attribute suggestions when inside of quotation marks
    
    * Restructed backend and endpoints to now point to a projectname/filename for fetching configurations.
    
    * Can now update xmlcontent in memory
    
    * Fixed issue with sidebar close button not properly showing up, moved savebutton to sidebar
    
    * Applied feedback from review
    
    * Forgot two instances of assuming a project was loaded in when that was not necessarily true, also removed the default tab
    
    * Added first few unit tests
    
    * Added unit tests for projectsettings and configuration
    
    * Added ProjectServiceTest and created custom exceptions for better exceptionhandling in the backend
    
    * Moved enabling and disabling filter logic to service instead of controller, removed 'test' package which was no longer in use
    
    * Finished writing service and controller tests
    
    * Merge conflict resolved
    
    * Made DTO's records instead of regular classes
    
    * Fixed test failing because of merged code

--- src/main/java/org/frankframework/flow/project/ProjectController.java
+++ src/main/java/org/frankframework/flow/project/ProjectController.java
@@ -2,9 +2,8 @@ package org.frankframework.flow.project;
 
 import org.frankframework.flow.configuration.Configuration;
 import org.frankframework.flow.configuration.ConfigurationDTO;
-
-import org.springframework.http.HttpStatus;
-import org.frankframework.flow.projectsettings.FilterType;
+import org.frankframework.flow.configuration.ConfigurationNotFoundException;
+import org.frankframework.flow.projectsettings.InvalidFilterTypeException;
 import org.frankframework.flow.configuration.Configuration;
 import org.frankframework.flow.configuration.ConfigurationDTO;
 import org.frankframework.flow.utility.XmlValidator;
@@ -37,62 +36,41 @@ public class ProjectController {
 		List<ProjectDTO> projectDTOList = new ArrayList<>();
 		List<Project> projects = projectService.getProjects();
 
-		for (Project project : projects)  {
-			ProjectDTO projectDTO = new ProjectDTO();
-			projectDTO.name = project.getName();
-			ArrayList<String> filenames = new ArrayList<>();
-			for (Configuration c :  project.getConfigurations()) {
-				filenames.add(c.getFilename());
-			}
-			projectDTO.filenames = filenames;
-			projectDTO.filters = project.getProjectSettings().getFilters();
-			projectDTOList.add(projectDTO);
+		for (Project project : projects) {
+			ProjectDTO dto = ProjectDTO.from(project);
+			projectDTOList.add(dto);
 		}
 		return ResponseEntity.ok(projectDTOList);
 	}
 
-	@GetMapping("/{projectname}")
-	public ResponseEntity<ProjectDTO> getProject(@PathVariable String projectname)  {
-		try {
-			Project project = projectService.getProject(projectname);
-			if (project == null) {
-				return ResponseEntity.notFound().build();
-			}
-			ProjectDTO projectDTO = new ProjectDTO();
-			projectDTO.name = project.getName();
-			ArrayList<String> filenames = new ArrayList<>();
-			for (Configuration c :  project.getConfigurations()) {
-				filenames.add(c.getFilename());
-			}
-			projectDTO.filenames = filenames;
-			projectDTO.filters = project.getProjectSettings().getFilters();
-			return ResponseEntity.ok(projectDTO);
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().build();
-		}
+	@GetMapping("/{projectName}")
+	public ResponseEntity<ProjectDTO> getProject(@PathVariable String projectName)
+			throws ProjectNotFoundException {
+
+		Project project = projectService.getProject(projectName);
+
+		ProjectDTO dto = ProjectDTO.from(project);
+
+		return ResponseEntity.ok(dto);
 	}
 
 	@GetMapping("/{projectName}/{filename}")
 	public ResponseEntity<ConfigurationDTO> getConfiguration(
 			@PathVariable String projectName,
-			@PathVariable String filename) {
+			@PathVariable String filename)
+			throws ProjectNotFoundException, ConfigurationNotFoundException {
 
 		Project project = projectService.getProject(projectName);
-		if (project == null) {
-			return ResponseEntity.notFound().build();
-		}
 
-		// Find configuration by filename
-		for (var config : project.getConfigurations()) {
+		for (Configuration config : project.getConfigurations()) {
 			if (config.getFilename().equals(filename)) {
-				ConfigurationDTO dto = new ConfigurationDTO();
-				dto.name = config.getFilename();
-				dto.xmlContent = config.getXmlContent();
+				ConfigurationDTO dto = new ConfigurationDTO(config.getFilename(), config.getXmlContent());
 				return ResponseEntity.ok(dto);
 			}
 		}
 
-		return ResponseEntity.notFound().build(); // No matching config found
+		throw new ConfigurationNotFoundException(
+				"Configuration with filename: " + filename + " cannot be found");
 	}
 
 	@PutMapping("/{projectName}/{filename}")
@@ -100,115 +78,47 @@ public class ProjectController {
 			@PathVariable String projectName,
 			@PathVariable String filename,
 			@RequestBody ConfigurationDTO configurationDTO) {
-		try {
-			String validationError = XmlValidator.validateXml(configurationDTO.xmlContent);
-			if (validationError != null) {
-				return ResponseEntity
-						.badRequest()
-						.body(new ErrorDTO("Invalid XML Content", validationError));
-			}
 
-			boolean updated = projectService.updateConfigurationXml(
-					projectName, filename, configurationDTO.xmlContent);
-
-			if (!updated) {
-				String notFoundMessage = String.format("Project with name %s or file with name $s can not be found", projectName, filename);
-				return ResponseEntity
-						.status(HttpStatus.NOT_FOUND)
-						.body(new ErrorDTO("Not found", notFoundMessage));
-			}
-
-			return ResponseEntity.ok().build();
-		} catch (Exception e) {
-			e.printStackTrace();
+		String validationError = XmlValidator.validateXml(configurationDTO.xmlContent());
+		if (validationError != null) {
 			return ResponseEntity
-					.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(new ErrorDTO("Invalid XML", e.getMessage()));
+					.badRequest()
+					.body(new ErrorDTO("Invalid XML Content", validationError));
 		}
 
+		projectService.updateConfigurationXml(projectName, filename, configurationDTO.xmlContent());
+
+		return ResponseEntity.ok().build();
 	}
 
 	@PostMapping("/{projectname}")
 	public ResponseEntity<ProjectDTO> createProject(@PathVariable String projectname) {
-		try {
-			projectService.createProject(projectname);
-			ProjectDTO projectDTO = new ProjectDTO();
-			projectDTO.name = projectname;
-			return ResponseEntity.ok(projectDTO);
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().build();
-		}
+		Project project = projectService.createProject(projectname);
+
+		ProjectDTO dto = ProjectDTO.from(project);
+
+		return ResponseEntity.ok(dto);
 	}
 
 	@PatchMapping("/{projectname}/filters/{type}/enable")
 	public ResponseEntity<ProjectDTO> enableFilter(
 			@PathVariable String projectname,
-			@PathVariable String type) {
-				try {
-			Project project = projectService.getProject(projectname);
-			if (project == null) {
-				return ResponseEntity.notFound().build();
-			}
+			@PathVariable String type)
+			throws ProjectNotFoundException, InvalidFilterTypeException {
 
-			// Parse enum safely
-			FilterType filterType = FilterType.valueOf(type.toUpperCase());
-
-			// Enable the filter
-			project.enableFilter(filterType);
-
-			// Return updated DTO
-			ProjectDTO dto = new ProjectDTO();
-			dto.name = project.getName();
-			ArrayList<String> filenames = new ArrayList<>();
-			for (Configuration c : project.getConfigurations()) {
-				filenames.add(c.getFilename());
-			}
-			dto.filenames = filenames;
-			dto.filters = project.getProjectSettings().getFilters();
-
-			return ResponseEntity.ok(dto);
-
-		} catch (IllegalArgumentException e) {
-			// thrown if invalid type string
-			return ResponseEntity.badRequest().body(null);
-		} catch (Exception e) {
-			return ResponseEntity.internalServerError().build();
-		}
+		Project project = projectService.enableFilter(projectname, type);
+		ProjectDTO dto = ProjectDTO.from(project);
+		return ResponseEntity.ok(dto);
 	}
 
 	@PatchMapping("/{projectname}/filters/{type}/disable")
 	public ResponseEntity<ProjectDTO> disableFilter(
 			@PathVariable String projectname,
-			@PathVariable String type) {
-				try {
-			Project project = projectService.getProject(projectname);
-			if (project == null) {
-				return ResponseEntity.notFound().build();
-			}
+			@PathVariable String type)
+			throws ProjectNotFoundException, InvalidFilterTypeException {
 
-			// Parse enum safely
-			FilterType filterType = FilterType.valueOf(type.toUpperCase());
-
-			// Disable the filter
-			project.disableFilter(filterType);
-
-			// Return updated DTO
-			ProjectDTO dto = new ProjectDTO();
-			dto.name = project.getName();
-			ArrayList<String> filenames = new ArrayList<>();
-			for (Configuration c : project.getConfigurations()) {
-				filenames.add(c.getFilename());
-			}
-			dto.filenames = filenames;
-			dto.filters = project.getProjectSettings().getFilters();
-
-			return ResponseEntity.ok(dto);
-
-		} catch (IllegalArgumentException e) {
-			// thrown if invalid type string
-			return ResponseEntity.badRequest().body(null);
-		} catch (Exception e) {
-			return ResponseEntity.internalServerError().build();
-		}
+		Project project = projectService.disableFilter(projectname, type);
+		ProjectDTO dto = ProjectDTO.from(project);
+		return ResponseEntity.ok(dto);
 	}
 }
