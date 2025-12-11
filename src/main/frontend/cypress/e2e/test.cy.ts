48b50316a303cdfae59f9ea36ba1a6472905fdcd
stijnpotters <stijn.potters1@gmail.com>
2025-12-11 12:00:07 +0100

    improved docker configurations

--- src/main/frontend/cypress/e2e/test.cy.ts
+++ src/main/frontend/cypress/e2e/test.cy.ts
@@ -1,9 +1,3 @@
 describe('Application Routing', () => {
-  beforeEach(() => {
-    cy.visit('/studio');
-  });
 
-  it('should went to studio', () => {
-    cy.url().should('include', '/studio');
-  });
 });
