37c681f936c6724681ae1fc78fea4a23a7109ce5
stijnpotters <stijn.potters1@gmail.com>
2025-12-11 11:43:50 +0100

    improved cypress test containers

--- src/test/java/org/frankframework/flow/cypress/RunCypressE2eTest.java
+++ src/test/java/org/frankframework/flow/cypress/RunCypressE2eTest.java
@@ -1,20 +1,13 @@
-package org.frankframework.flow;
+package org.frankframework.flow.cypress;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.github.wimdeblauwe.testcontainers.cypress.CypressContainer;
 import io.github.wimdeblauwe.testcontainers.cypress.CypressTestResults;
 import io.github.wimdeblauwe.testcontainers.cypress.CypressTestSuite;
 import jakarta.annotation.Nonnull;
-import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DynamicContainer;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.TestFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -26,10 +19,16 @@ import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.Stream;
-
-import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.frankframework.flow.FlowApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * Runs e2e tests with Cypress in a Testcontainer.
@@ -40,14 +39,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Sergi Philipsen
  * @see "https://github.com/wimdeblauwe/testcontainers-cypress"
  */
-@Log4j2
 @Testcontainers(disabledWithoutDocker = true)
 @Tag("integration")
 public class RunCypressE2eTest {
     private static final String SPRING_BASE_URL = "http://localhost:8080";
     private static final String TEST_CONTAINER_BASE_URL = "http://host.testcontainers.internal:8080";
-    private static final Path MOCHAWESOME_REPORTS_DIR = Paths
-            .get("target/test-classes/e2e/cypress/test-results/reports/mochawesome");
+    private static final Path MOCHAWESOME_REPORTS_DIR =
+            Paths.get("target/test-classes/e2e/cypress/test-results/reports/mochawesome");
 
     private static CypressContainer container;
     private static ConfigurableApplicationContext run;
@@ -72,11 +70,8 @@ public class RunCypressE2eTest {
     private static boolean isApplicationHealthy() {
         try {
             String url = SPRING_BASE_URL + "/actuator/health";
-            HttpRequest req = HttpRequest.newBuilder(URI.create(url))
-                    .GET()
-                    .build();
-            HttpResponse<String> resp = HttpClient.newHttpClient()
-                    .send(req, HttpResponse.BodyHandlers.ofString());
+            HttpRequest req = HttpRequest.newBuilder(URI.create(url)).GET().build();
+            HttpResponse<String> resp = HttpClient.newHttpClient().send(req, HttpResponse.BodyHandlers.ofString());
             return resp.statusCode() == 200 && resp.body().contains("UP");
         } catch (Exception e) {
             return false;
@@ -88,11 +83,11 @@ public class RunCypressE2eTest {
 
         container = new CypressContainer("cypress/included:15.0.0");
 
-        // It has to be like this and not use the builder
         container.withBaseUrl(TEST_CONTAINER_BASE_URL);
         container.withMochawesomeReportsAt(MOCHAWESOME_REPORTS_DIR);
         container.withClasspathResourcePath("e2e");
         container.withWorkingDirectory("/e2e/cypress");
+        container.withLogConsumer(new Log4j2LogConsumer("CypressContainer", "CYPRESS"));
 
         container.start();
         assertTrue(container.isRunning());
@@ -100,8 +95,7 @@ public class RunCypressE2eTest {
 
     @AfterAll
     static void tearDown() {
-        if (run == null)
-            return;
+        if (run == null) return;
 
         run.stop();
         container.stop();
@@ -118,9 +112,8 @@ public class RunCypressE2eTest {
         CypressTestResults testResults = container.getTestResults();
 
         if (testResults.getNumberOfFailingTests() > 0) {
-            throw new AssertionError(
-                    "Cypress tests failed! " + testResults.getNumberOfFailingTests() + " test(s) failed.\n\n"
-                            + testResults);
+            throw new AssertionError("Cypress tests failed! " + testResults.getNumberOfFailingTests()
+                    + " test(s) failed.\n\n" + testResults);
         }
 
         return testResults.getSuites().stream().map(this::createContainerFromSuite);
@@ -128,13 +121,12 @@ public class RunCypressE2eTest {
 
     private DynamicContainer createContainerFromSuite(CypressTestSuite suite) {
         Stream<DynamicTest> dynamicTests = suite.getTests().stream()
-                .map(test -> DynamicTest.dynamicTest(
-                        test.getDescription(), () -> {
-                            if (!test.isSuccess()) {
-                                assertTrue(isApplicationHealthy(), "!! application not reachable !!");
-                            }
-                            assertTrue(test.isSuccess(), test::getErrorMessage);
-                        }));
+                .map(test -> DynamicTest.dynamicTest(test.getDescription(), () -> {
+                    if (!test.isSuccess()) {
+                        assertTrue(isApplicationHealthy(), "!! application not reachable !!");
+                    }
+                    assertTrue(test.isSuccess(), test::getErrorMessage);
+                }));
 
         return DynamicContainer.dynamicContainer(suite.getTitle(), dynamicTests);
     }
