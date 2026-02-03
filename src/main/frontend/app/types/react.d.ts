55374e3ad412af501da457844936198fbed3d3ce
stijnpotters <stijn.potters1@gmail.com>
2026-02-03 18:42:50 +0100

    added comment with link to issue

--- src/main/frontend/app/types/react.d.ts
+++ src/main/frontend/app/types/react.d.ts
@@ -1,6 +1,7 @@
 import 'react'
 
 declare module 'react' {
+  // https://github.com/facebook/react/issues/3468
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface InputHTMLAttributes<T> {
     webkitdirectory?: 'true' | 'false'
