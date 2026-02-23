376b9393119b28c095c944c71484fa02b4d7b8a5
Daan0709 <daanvmaldegem@live.nl>
2026-02-23 14:21:40 +0100

    Updated test

--- src/test/java/org/frankframework/flow/xml/XmlControllerTest.java
+++ src/test/java/org/frankframework/flow/xml/XmlControllerTest.java
@@ -11,12 +11,14 @@ import org.frankframework.flow.project.InvalidXmlContentException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(XmlController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class XmlControllerTest {
 
     @Autowired
