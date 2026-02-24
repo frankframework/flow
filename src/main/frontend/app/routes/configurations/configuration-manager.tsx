28f673908a7e15164cdd28fcd926307a5c18ee8c
Daan0709 <daanvmaldegem@live.nl>
2026-02-24 19:48:53 +0100

    Slight styling change

--- src/main/frontend/app/routes/configurations/configuration-manager.tsx
+++ src/main/frontend/app/routes/configurations/configuration-manager.tsx
@@ -205,7 +205,7 @@ export default function ConfigurationManager() {
         <Search value={searchQuery} onChange={handleSearch} />
       </div>
 
-      <div className="flex flex-wrap gap-4">
+      <div className="border-border bg-backdrop flex flex-wrap gap-4 rounded border p-4">
         <AnimatePresence>
           {filteredConfigurations.map((file) => (
             <motion.div
