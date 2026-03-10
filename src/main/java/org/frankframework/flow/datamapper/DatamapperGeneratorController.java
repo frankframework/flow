c4aa9f82c8b671256ba7d69ffb0e0cfb809e72b0
VincentvanMedenbach <31502129+VincentvanMedenbach@users.noreply.github.com>
2026-03-10 09:20:19 +0100

    Datamapper xslt generator (#324)
    * Reworked the export function to reflect mappingstructure better
    
    * Working on making a simple XSLT generator prototype
    
    * Implemented first prototype version of xslt generation
    
    * Changed import to use root elements properly
    
    * Changed mapping generator service to allow for a file to be passed to it
    
    * Implemented support for JSON --> XML
    
    * Added support for data conversions
    
    * Added support for objects in the input,  added back source table in frontend output
    
    * Removed console log, removed comments
    
    * Reworked the datamapper generator to use fileSystemStorage
    
    * Cleaned up generator XSLT file
    
    * Reworked function to use outputxsl instead of xsl elements
    
    WOOOOO from 279 lines to 153
    
    * Removed alot of whitespace
    
    * Connected frontend to backend generation function
    
    * Maven spotless apply
    
    * Added assert to basic generation
    
    * Added deletion of temporarily config file, Implemented extra tests
    
    * Removed unused throws
    
    * Updated .tsx to .ts
    
    * Added extra exception type, improved try/catch, removed comment
    
    * i forgot to run spotless again...
    
    * Exceptions --> ApiException :D
    
    Did not forget spotless this time

---
+++ src/main/java/org/frankframework/flow/datamapper/DatamapperGeneratorController.java
@@ -0,0 +1,23 @@
+package org.frankframework.flow.datamapper;
+
+import org.frankframework.flow.exception.ApiException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/datamapper/")
+public class DatamapperGeneratorController {
+    private final DatamapperGeneratorService datamapperGeneratorService;
+
+    public DatamapperGeneratorController(DatamapperGeneratorService datamapperGeneratorService) {
+        this.datamapperGeneratorService = datamapperGeneratorService;
+    }
+
+    @PutMapping("/{projectName}/generate")
+    public ResponseEntity<Void> updateConfiguration(@PathVariable String projectName, @RequestBody String content)
+            throws ApiException {
+
+        datamapperGeneratorService.generateFromProject(projectName, content);
+        return ResponseEntity.ok().build();
+    }
+}
