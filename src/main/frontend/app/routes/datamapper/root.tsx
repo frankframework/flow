c59015c26093984a2e2f238b2779cc52b6d1bbd0
VincentvanMedenbach <31502129+VincentvanMedenbach@users.noreply.github.com>
2026-03-23 12:01:51 +0100

    Reworked the application to have a better flow, moved generation of xslt to properties page and added unset highlights (#369)
    * Reworked the application to have a better flow, moved generation of xslt to properties page and added unset highlights
    
    * Fixed small routing bug
    
    * Fixed eslint rule

--- src/main/frontend/app/routes/datamapper/root.tsx
+++ src/main/frontend/app/routes/datamapper/root.tsx
@@ -1,28 +1,22 @@
 import { useEffect, useReducer, useState } from 'react'
 
 import PropertyList from './property-list'
-import { showErrorToast, showInfoToast, showSuccessToast } from '~/components/toast'
+import { showErrorToast, showSuccessToast } from '~/components/toast'
 import {
   DEFAULT_MAPPING_LIST_CONFIG,
   mappingListConfigReducer,
 } from '~/stores/datamapper_state/mappingListConfig/reducer'
-import { FLOW_KEY } from '~/utils/datamapper_utils/const'
-import { convertMappingConfigToMappingFile } from '~/utils/datamapper_utils/convert-node-utils'
+
 import AdvancedEditor from './advanced-editor'
 import Initialize from './initialize'
 import MappingTable from './mapping-table'
 import { ReactFlowProvider } from '@xyflow/react'
 import { FileProvider } from '~/stores/datamapper_state/schemaQueue/schema-queue-context'
 import Button from '~/components/inputs/button'
-import {
-  saveDatamapperConfiguration,
-  fetchDatamapperConfiguration,
-  generateDatamapperXSLT,
-} from '~/services/datamapper-service'
+import { saveDatamapperConfiguration, fetchDatamapperConfiguration } from '~/services/datamapper-service'
 import { useProjectStore } from '~/stores/project-store'
 
 export default function Root() {
-  const [route, setRoute] = useState('Initialize')
   const routes = ['Initialize', 'Properties', 'Mappings', 'Advanced']
 
   const project = useProjectStore.getState().project
@@ -31,16 +25,22 @@ export default function Root() {
     mappingListConfigReducer,
     DEFAULT_MAPPING_LIST_CONFIG,
   )
+  const [route, setRoute] = useState('Initialize')
 
   useEffect(() => {
     const loadConfig = async () => {
       if (project) {
         const config = await fetchDatamapperConfiguration(project.name)
+        if (config) {
+          dispatchMappingListConfig({
+            type: 'IMPORT_CONFIG',
+            payload: config,
+          })
 
-        dispatchMappingListConfig({
-          type: 'IMPORT_CONFIG',
-          payload: config,
-        })
+          setRoute(config.stage == 'Mapping' ? 'Properties' : 'Initialize')
+        } else {
+          setRoute('Initialize')
+        }
       }
     }
 
@@ -56,6 +56,7 @@ export default function Root() {
   }
 
   const [confirmed, setConfirmed] = useState<boolean>(mappingListConfig.stage != 'INIT')
+
   useEffect(() => {
     setConfirmed(mappingListConfig.stage !== 'INIT')
   }, [mappingListConfig.stage])
@@ -92,28 +93,7 @@ export default function Root() {
           >
             Test External node log
           </button>
-          <button
-            className="border-border hover:bg-hover active:bg-selected w-48 rounded-md border bg-red-500 px-4 py-2 text-sm"
-            onClick={async () => {
-              if (!project) {
-                showErrorToast('Not in valid project!')
-                return
-              }
 
-              try {
-                await generateDatamapperXSLT(
-                  project.name,
-                  JSON.stringify(convertMappingConfigToMappingFile(mappingListConfig)),
-                )
-
-                showInfoToast('Exporting file!', 'Debug')
-              } catch (error) {
-                showErrorToast(error instanceof Error ? error.message : String(error))
-              }
-            }}
-          >
-            Export as final mappingFile
-          </button>
           <button
             className="border-border hover:bg-hover active:bg-selected hidden w-48 rounded-md border bg-red-500 px-4 py-2 text-sm"
             onClick={handleManualConfigExport}
@@ -132,7 +112,7 @@ export default function Root() {
         </div>
 
         {/* Center modal/container */}
-        <div className="bg-backdrop inset-0 z-50 h-full items-center justify-center p-4">
+        <div className="bg-backdrop inset-0 z-50 h-screen items-center justify-center">
           <div className="bg-background text-foreground rounded-lg shadow-lg">
             {/* Header */}
             <div className="border-b-border box-border h-10 w-full rounded-t-md border-b bg-[radial-gradient(ellipse_farthest-corner_at_20%_20%,var(--type-pipe)_0%,var(--color-background)_100%)] p-2 text-lg font-semibold">
@@ -140,26 +120,26 @@ export default function Root() {
             </div>
 
             {/* Route buttons */}
-            <div className="my-3 flex h-10 w-full gap-4 px-4">
-              {routes.map((routeName) => (
-                <Button
-                  key={routeName}
-                  disabled={routeName !== 'Initialize' && !confirmed}
-                  className="flex-1"
-                  onClick={() => setRoute(routeName)}
-                >
-                  {routeName}
-                </Button>
-              ))}
-            </div>
+            {mappingListConfig.stage == 'Mapping' && (
+              <div className="my-3 flex h-10 w-full gap-4 px-4">
+                {routes
+                  .filter((routeName) => routeName != 'Initialize')
+                  .map((routeName) => (
+                    <Button key={routeName} className="flex-1" onClick={() => setRoute(routeName)}>
+                      {routeName}
+                    </Button>
+                  ))}
+              </div>
+            )}
 
             {/* Main content */}
-            <div className="w-full overflow-auto p-4" style={{ maxHeight: 'calc(96vh - 93px)' }}>
+            <div className="w-full overflow-auto" style={{ maxHeight: 'calc(96vh - 93px)' }}>
               {route === 'Initialize' && (
                 <Initialize
                   config={mappingListConfig}
                   configDispatch={dispatchMappingListConfig}
                   confirmed={confirmed}
+                  setRoute={setRoute}
                 />
               )}
               {route === 'Properties' && (
