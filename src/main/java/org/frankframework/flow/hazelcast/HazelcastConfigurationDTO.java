8dbbe223809e6ff35c9f0b79d4174f20820d0176
stijnpotters <stijn.potters1@gmail.com>
2026-03-24 13:36:25 +0100

    fix: improved hazelcast connection logic

--- src/main/java/org/frankframework/flow/hazelcast/HazelcastConfigurationDTO.java
+++ src/main/java/org/frankframework/flow/hazelcast/HazelcastConfigurationDTO.java
@@ -2,22 +2,5 @@ package org.frankframework.flow.hazelcast;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class HazelcastConfigurationDTO {
-
-	private @Getter @Setter String name;
-	private @Getter @Setter String version;
-	private @Getter @Setter boolean stubbed;
-	private @Getter @Setter String type;
-	private @Getter @Setter String directory;
-
-	private @Getter @Setter String parent;
-
-	@Override
-	public String toString() {
-		return name;
-	}
-}
+public record HazelcastConfigurationDTO(String name, String directory) {}
--- src/main/java/org/frankframework/flow/hazelcast/HazelcastController.java
+++ src/main/java/org/frankframework/flow/hazelcast/HazelcastController.java
@@ -1,9 +1,7 @@
 package org.frankframework.flow.hazelcast;
 
 import java.util.List;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
