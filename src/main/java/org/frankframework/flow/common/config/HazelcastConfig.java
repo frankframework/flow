dba25f586201c6ebd65cc24724d9fbfd1abaed49
stijnpotters <stijn.potters1@gmail.com>
2026-03-24 13:36:25 +0100

    fix: improved hazelcast connection logic

--- src/main/java/org/frankframework/flow/common/config/HazelcastConfig.java
+++ src/main/java/org/frankframework/flow/common/config/HazelcastConfig.java
@@ -1,47 +1,41 @@
 package org.frankframework.flow.common.config;
 
 import org.apache.commons.lang3.StringUtils;
-
 import org.frankframework.management.bus.LocalGateway;
 import org.frankframework.management.bus.OutboundGatewayFactory;
 import org.frankframework.management.gateway.HazelcastOutboundGateway;
 import org.frankframework.management.security.JwtKeyGenerator;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.integration.channel.PublishSubscribeChannel;
-import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 @Configuration
 @ConditionalOnProperty(name = "hazelcast.enabled", havingValue = "true")
-public class AnnotationConfig {
+public class HazelcastConfig {
 
-	private final ApplicationContext applicationContext;
-
-	public AnnotationConfig(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
+	@Value("${configurations.directory:}")
+	private String configurationsDirectory;
 
 	@Bean
 	public JwtKeyGenerator jwtKeyGenerator() {
 		return new JwtKeyGenerator();
 	}
 
-	@Bean(name = "frank-management-bus")
-	@ConditionalOnProperty(name = "configurations.directory", matchIfMissing = false)
-	public MessageChannel frankManagementBus() {
+	@Bean("frank-management-bus")
+	@ConditionalOnProperty(name = "configurations.directory")
+	public SubscribableChannel frankManagementBus() {
 		return new PublishSubscribeChannel();
 	}
 
 	@Bean
-	@Scope("singleton")
-	public OutboundGatewayFactory createOutboundGatewayFactory() {
+	public OutboundGatewayFactory outboundGatewayFactory() {
 		OutboundGatewayFactory factory = new OutboundGatewayFactory();
-		String configDirectory = applicationContext.getEnvironment().getProperty("configurations.directory");
-		String gatewayClassName = StringUtils.isEmpty(configDirectory) ? HazelcastOutboundGateway.class.getCanonicalName() : LocalGateway.class.getCanonicalName();
+		String gatewayClassName = StringUtils.isNotBlank(configurationsDirectory)
+				? LocalGateway.class.getCanonicalName()
+				: HazelcastOutboundGateway.class.getCanonicalName();
 		factory.setGatewayClassname(gatewayClassName);
 		return factory;
 	}
