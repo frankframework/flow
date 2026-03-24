d03aff341a41467b1f88375c2839f8c57024d1af
stijnpotters <stijn.potters1@gmail.com>
2026-03-24 14:45:51 +0100

    fix: improved LocalGateway logic, added tests and improved code more

--- src/main/java/org/frankframework/flow/FlowWarInitializer.java
+++ src/main/java/org/frankframework/flow/FlowWarInitializer.java
@@ -15,7 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import org.jspecify.annotations.NullMarked;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
