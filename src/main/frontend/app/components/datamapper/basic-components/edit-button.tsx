78b5301ab47dc046421c84ee32ce44cd873a2a2b
Vincent <vincent1906@live.nl>
2026-03-27 16:37:35 +0100

    Fixed wrong import

--- src/main/frontend/app/components/datamapper/basic-components/edit-button.tsx
+++ src/main/frontend/app/components/datamapper/basic-components/edit-button.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import TextCircle from '/icons/solar/Text circle.svg?react'
+import TextCircle from '/icons/solar/Text Circle.svg?react'
 
 export default function EditButton({ onClick, className }: { onClick: () => void; className?: string }) {
   return (
