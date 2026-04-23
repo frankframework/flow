97bb3727be5c127204acf6f945c426e611757c9d
Stijn Potters <stijn.potters1@gmail.com>
2026-04-23 08:46:29 +0200

    Update error toast styling for improved visibility (#446)
    * Update error toast styling for improved visibility
    
    * applied linting

--- src/main/frontend/app/components/toast.tsx
+++ src/main/frontend/app/components/toast.tsx
@@ -16,7 +16,7 @@ const defaultStyle = 'items-end justify-end pointer-events-none'
 const toastStyles = {
   ERROR: {
     container: defaultStyle,
-    card: `${toastBaseCard} bg-error-background`,
+    card: `${toastBaseCard} bg-error`,
     icon: '',
     defaultDuration: 2000,
   },
@@ -98,10 +98,7 @@ export function Toast() {
 
   return createPortal(
     <div className={clsx('fixed inset-0 z-50 flex px-4', styles.container)} onClick={hideToast} data-theme={theme}>
-      <div
-        className={clsx('pointer-events-auto', toast.type !== 'ERROR' && 'mr-4 mb-4')}
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className="pointer-events-auto mr-4 mb-4" onClick={(e) => e.stopPropagation()}>
         <div className={clsx(styles.card)}>
           <div className="flex items-start gap-3 text-xl">
             <div className="flex flex-col">
