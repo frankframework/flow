29f3b8e1978cc09cd5d4279e671fba35f13db253
Stijn Potters <stijn.potters1@gmail.com>
2026-05-06 12:24:37 +0200

    Update src/main/frontend/app/routes/studio/studio.tsx
    Co-authored-by: Vivy <4380412+Matthbo@users.noreply.github.com>
    Signed-off-by: Stijn Potters <stijn.potters1@gmail.com>

--- src/main/frontend/app/routes/studio/studio.tsx
+++ src/main/frontend/app/routes/studio/studio.tsx
@@ -81,7 +81,7 @@ export default function Studio() {
   const setVisibility = useSidebarStore((state) => state.setVisibility)
   const [showNodeContext, setShowNodeContext] = useState(false)
   const { nodeId, editingSubtype, isMultiSelect } = useNodeContextStore(
-    useShallow((s) => ({ nodeId: s.nodeId, editingSubtype: s.editingSubtype, isMultiSelect: s.isMultiSelect })),
+    useShallow((nodeContext) => ({ nodeId: nodeContext.nodeId, editingSubtype: nodeContext.editingSubtype, isMultiSelect: nodeContext.isMultiSelect })),
   )
 
   const { activeTab, activeTabPath } = useTabStore(
