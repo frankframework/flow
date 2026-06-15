3355b90b72008f0c8b82bc1f57405c01c160840b
stijnpotters <stijn.potters1@gmail.com>
2026-06-15 12:52:28 +0200

    Update development environment configuration and enhance security settings

--- src/main/frontend/environment/development.ts
+++ src/main/frontend/environment/development.ts
@@ -2,7 +2,7 @@ import base, { type EnvironmentVariables } from './base'
 
 const development: EnvironmentVariables = {
   ...base,
-  apiBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: '',
 }
 
 export default development
