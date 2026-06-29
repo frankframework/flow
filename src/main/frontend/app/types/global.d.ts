4d093971ddba0499e01a138cb5b0f773c1406c8c
stijnpotters <stijn.potters1@gmail.com>
2026-06-29 15:32:30 +0200

    chore: removed dead showDirectoryPicker code piece

--- src/main/frontend/app/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-﻿// Must stay an `interface` to augment the global `Window` (declaration merging).
-// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-interface Window {
-  showDirectoryPicker(): Promise<FileSystemDirectoryHandle>
-}
