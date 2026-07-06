fa21904b6d158c35cf738284a3b3fcb07a648371
stijnpotters <stijn.potters1@gmail.com>
2026-07-06 17:28:47 +0200

    Add alias resolution for doc-library packages in Vitest configuration so tests will work

--- src/main/frontend/vitest.config.ts
+++ src/main/frontend/vitest.config.ts
@@ -1,9 +1,19 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const docLibrary = (FFPackage: string, entry: string) =>
+  fileURLToPath(new URL(`node_modules/@frankframework/${FFPackage}/${entry}`, import.meta.url))
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@frankframework/doc-library-core': docLibrary('doc-library-core', 'dist/doc-library-core.js'),
+      '@frankframework/doc-library-react': docLibrary('doc-library-react', 'doc-library-react.js'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
